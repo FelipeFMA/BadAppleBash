a2795e983edfd04df8d032140a87c38ce65b8754
Felipe Avelar <30672253+FelipeFMA@users.noreply.github.com>
2024-05-08 15:26:17 -0300

    made the script smarter

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 # Fork by: Felipe Avelar
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-echo "The directory of the currently executing script is: ${SCRIPT_DIR}"
+
+echo
 
 if [ "$1" ]; then
   speed=$1
@@ -13,10 +14,18 @@ fi
 
 read -p "Do you want to use mpv to play sound? You need mpv installed to do that. (y/n): " choice
 if [[ $choice =~ ^[Yy]$ ]]; then
+  # Check if mpv is installed
+  if ! command -v mpv &> /dev/null; then
+    echo
+    echo "mpv is not installed. Please install it to use this feature."
+    exit 1
+  fi
   mpv --no-video bad_apple.mp4 > /dev/null 2>&1 &
 fi
 
-dir="$SCRIPT_DIR/frames-ascii" 
+dir="$SCRIPT_DIR/frames-ascii"
+
+clear
 
 for filename in $(ls -v "$dir"); do
     tput cup 0 0
