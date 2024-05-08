c66f2498006cad25783b794f330b2cc7f809ae98
Felipe Avelar <30672253+FelipeFMA@users.noreply.github.com>
2024-05-08 15:39:13 -0300

    removed speed
    who on earth would use that

--- run.sh
+++ run.sh
@@ -4,13 +4,19 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-echo
+usage() {
+    echo
+    echo "Usage: $0"
+    echo "  -h, --help  Display this help message"
+    echo
+    exit 1
+}
 
-if [ "$1" ]; then
-  speed=$1
-else
-  speed=1.0
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    usage
 fi
+
+echo
 
 read -p "Do you want to use mpv to play sound? You need mpv installed to do that. (y/n): " choice
 if [[ $choice =~ ^[Yy]$ ]]; then
@@ -18,6 +24,7 @@ if [[ $choice =~ ^[Yy]$ ]]; then
   if ! command -v mpv &> /dev/null; then
     echo
     echo "mpv is not installed. Please install it to use this feature."
+    echo
     exit 1
   fi
   mpv --no-video bad_apple.mp4 > /dev/null 2>&1 &
