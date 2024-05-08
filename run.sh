91d106f969b57a5717576d32c70f808fe5042dfa
Felipe Avelar <30672253+FelipeFMA@users.noreply.github.com>
2024-05-08 13:53:41 -0300

    added the intro video and the run script
    still misses all the ascii art

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Original code: Nguyen Khac Trung Kien
+# Fork by: Felipe Avelar
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+echo "The directory of the currently executing script is: ${SCRIPT_DIR}"
+
+if [ "$1" ]; then
+  speed=$1
+else
+  speed=1.0
+fi
+
+mpv --no-video bad_apple.mp4 > /dev/null 2>&1 &
+dir="$SCRIPT_DIR/frames-ascii" 
+
+for filename in $(ls -v "$dir"); do
+#    clear
+    file="$dir/$filename"
+    if [ -f "$file" ]; then
+        cat "$file"
+    fi
+
+#    sleep $((0.02 / $speed))
+    sleep 0.024
+
+
+done
+
